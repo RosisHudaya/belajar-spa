a0da280bf4adaeffcf939ce4dfb2c003c91c4e10
RosisHudaya <rosishudayaputra02@gmail.com>
2023-02-13 18:22:24 +0700

    side effect

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 let state = {
-    inputValue: "",
+    inputValue: localStorage.getItem("inputValue" ?? ""),
     hash: location.hash,
 };
 
 function setState(newState) {
-    state = { ...state, ...newState };
+    const prevState = {...state};
+    const nextState = { ...state, ...newState };
+    state = nextState;
     render();
+    onStateChange(prevState, newState);
 };
+
+function onStateChange(prevState, nextState) {
+    if (prevState.inputValue != nextState.inputValue) {
+        localStorage.setItem("inputValue", nextState.inputValue);
+    }
+
+    if (prevState.hash != nextState.hash) {
+        history.pushState(null, "", nextState.hash);
+    }
+}
 
 function Link(props) {
     const link = document.createElement("a");
@@ -15,8 +28,7 @@ function Link(props) {
     link.onclick = function (event) {
         event.preventDefault();
         const url = new URL(event.target.href);
-        setState({ hash: url.hash});
-        history.pushState(null, "", event.target.href);
+        setState({ hash: url.hash });
         render();
     };
 
