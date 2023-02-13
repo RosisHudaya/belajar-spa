eebb099171ec4607631ed155bd45c4362ce9665e
RosisHudaya <rosishudayaputra02@gmail.com>
2023-02-13 17:50:02 +0700

    state

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+let state = {
+    inputValue: "",
+    hash: location.hash,
+};
+
+function setState(newState) {
+    state = { ...state, ...newState };
+    render();
+};
+
 function Link(props) {
     const link = document.createElement("a");
     link.href = props.href;
     link.textContent = props.label;
     link.onclick = function (event) {
         event.preventDefault();
+        const url = new URL(event.target.href);
+        setState({ hash: url.hash});
         history.pushState(null, "", event.target.href);
         render();
     };
@@ -12,8 +24,8 @@ function Link(props) {
 }
 
 function Navbar() {
-    const linkHome = Link({ href: "#home", label: "Home"});
-    const linkAbout = Link({ href: "#about", label: "About"});
+    const linkHome = Link({ href: "#home", label: "Home" });
+    const linkAbout = Link({ href: "#about", label: "About" });
 
     const div = document.createElement("div");
     div.append(linkHome);
@@ -25,24 +37,34 @@ function Navbar() {
 function HomeScreen() {
     const navbar = Navbar();
     const textPreview = document.createElement("p");
+    textPreview.textContent = state.inputValue;
 
     const input = document.createElement("input");
+    input.id = "input";
+    input.value = state.inputValue;
     input.oninput = function (event) {
-        textPreview.textContent = event.target.value;
+        setState({ inputValue: event.target.value });
     };
 
     input.placeholder = "Enter your name";
 
+    const buttonClear = document.createElement("button");
+    buttonClear.textContent = "Clear";
+    buttonClear.onclick = function () {
+        setState({ inputValue: "" });
+    };
+
     const div = document.createElement("div");
     div.append(navbar);
     div.append(input);
+    div.append(buttonClear);
     div.append(textPreview);
 
     return div;
 }
 
 function AboutScreen() {
-    const linkHome = Link({ href: "#home", label: "Back to Home"});
+    const linkHome = Link({ href: "#home", label: "Back to Home" });
 
     const text = document.createElement("p");
     text.textContent = "Welcome to About";
@@ -58,9 +80,9 @@ function App() {
     const homeScreen = HomeScreen();
     const aboutScreen = AboutScreen();
 
-    if (location.hash == "#about") {
+    if (state.hash == "#about") {
         return aboutScreen;
-    } else if (location.hash == "#home") {
+    } else if (state.hash == "#home") {
         return homeScreen;
     }
 }
@@ -68,8 +90,20 @@ function App() {
 function render() {
     const root = document.getElementById("root");
     const app = App();
+
+    const focusedElementId = document.activeElement.id;
+    const focusedElementSelectionStart = document.activeElement.selectionStart;
+    const focusedElementSelectionEnd = document.activeElement.selectionEnd;
+
     root.innerHTML = "";
     root.append(app);
+
+    if (focusedElementId) {
+        const focusedElement = document.getElementById(focusedElementId);
+        focusedElement.focus();
+        focusedElement.selectionStart = focusedElementSelectionStart;
+        focusedElement.selectionEnd = focusedElementSelectionEnd;
+    }
 }
 
 render();
